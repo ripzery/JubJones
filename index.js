8cba088ffcbd71ada0da3139ace7da7d19b36223
euro <phuchit@omise.co>
2017-11-16 14:37:18 +0700

    Sleep when sleep increase and Capture when capture increase

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const { exec } = require('child_process');
 const admin = require("firebase-admin");
 const { machineId, machineIdSync } = require('node-machine-id');
 const serviceAccount = require("./jubjones-omg-firebase-adminsdk-vqwk5-4353d61f82.json");
+let sleep = 0
+let capture = 0
 
 async function getMachineId() {
     let id = await machineId();
 }
 
-const capture = () => {
+const takePic = () => {
     exec('imagesnap -w 1 jones.png', (err, stdout, stderr) => {
         if (err) {
             return
@@ -43,14 +45,26 @@ function listen() {
     let id = machineIdSync({ original: true })
     console.log(id)
     let userRef = db.ref('/').child(`users/${id}`)
-    userRef.on("value", function (snapshot) {
-        console.log(snapshot.val());
-    }, function (errorObject) {
-        console.log('Reading failed : ' + errorObject.code)
-    })
+
     userRef.set({
         sleep: 0,
         capture: 0
+    })
+
+    userRef.on("value", function (snapshot) {
+        let sSleep = snapshot.val().sleep
+        let sCapture = snapshot.val().capture
+        if (sSleep > sleep) {
+            lockscreen()
+            sleep = sSleep
+        }
+        if (sCapture > capture) {
+            takePic()
+            capture = sCapture
+        }
+        console.log(snapshot.val());
+    }, function (errorObject) {
+        console.log('Reading failed : ' + errorObject.code)
     })
 }
 
